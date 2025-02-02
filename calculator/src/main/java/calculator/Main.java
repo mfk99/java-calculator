269a07c105a5b73e673cc0d14d98a3d3db6a2947
mfk99 <matti.kahkonen99@gmail.com>
2025-02-02 22:32:55 +0200

    streamline launch process

--- calculator/src/main/java/calculator/Main.java
+++ calculator/src/main/java/calculator/Main.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.ui.CalculatorUI;
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        App.launch(args);
+        Application.launch(CalculatorUI.class, args); // Launch JavaFX UI
     }
 }
