e7822f31e73f3676d5ba4a7a13604f4bc4233c9d
mfk99 <matti.kahkonen99@gmail.com>
2025-02-02 14:01:47 +0200

    add logic to new operations

--- calculator/src/main/java/calculator/Calculator.java
+++ calculator/src/main/java/calculator/Calculator.java
@@ -1,8 +1,10 @@
 package calculator;
 
 import calculator.operations.Operation;
+import calculator.operations.SquareRoot;
 import calculator.operations.Addition;
 import calculator.operations.Division;
+import calculator.operations.Exponent;
 import calculator.operations.Multiplication;
 import calculator.operations.Subtraction;
 
@@ -11,12 +13,16 @@ public class Calculator {
   private Operation subtraction;
   private Operation multiplication;
   private Operation division;
+  private Operation exponent;
+  private Operation squareRoot;
 
   public Calculator() {
     this.addition = new Addition();
     this.subtraction = new Subtraction();
     this.multiplication = new Multiplication();
     this.division = new Division();
+    this.exponent = new Exponent();
+    this.squareRoot = new SquareRoot();
   }
 
   public double calculate(double a, double b, String operator) {
@@ -29,6 +35,10 @@ public class Calculator {
         return multiplication.operate(a, b);
       case "/":
         return division.operate(a, b);
+      case "^":
+        return exponent.operate(a, b);
+      case "sqrt":
+        return squareRoot.operate(a, b);
       default:
         throw new IllegalArgumentException("Invalid operator");
     }
