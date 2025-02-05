6d4c22ef623c6f0e292a4cd6edb2d15687abbabc
mfk99 <matti.kahkonen99@gmail.com>
2025-02-05 12:09:27 +0200

    remove unused imports

--- calculator/src/main/java/calculator/ui/CalculatorUI.java
+++ calculator/src/main/java/calculator/ui/CalculatorUI.java
@@ -1,10 +1,7 @@
 package calculator.ui;
 
-import calculator.controller.CalculatorController;
-import calculator.ui.ButtonFactory;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
